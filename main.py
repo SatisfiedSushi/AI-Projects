e5c0a323043a466a065c50cb55e9883bdbed2d47
SatisfiedSushi <71732695+SatisfiedSushi@users.noreply.github.com>
2022-11-02 14:17:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import AIs.GateGuesser.GateGuesser as GateGuesser
 import NeuralNetwork
-
+import numpy as np
+import random
 # GateGuesser.GateGuesser()
+x = random.randint(0,1)
+y = random.randint(0,1)
+print("x: " + str(x))
+print("y: " + str(y))
+
+actual = []
+if x == 0:
+  actual.append(1)
+else:
+  actual.append(0)
+if y == 0:
+  actual.append(1)
+else:
+  actual.append(0)
+
 NN = NeuralNetwork.NeuralNetwork(2, [2, 2], 2)
-NN.set_input_layer([-0.7, 0.4])
+NN.set_input_layer([x, y])
 prediction = NN.network_forward_pass()
 
 for i in NN.input_layer:
     print(i.inputs)
+    print("weights: " + str(i.weights))
 
-for i in NN.hidden_layers[0]:
-    print(i.inputs)
+for j in NN.hidden_layers:
+    for i in j:
+      print(i.inputs)
+      print("weights: " + str(i.weights))
 
 for i in NN.output_layer:
     print(i.inputs)
+    print("weights: " + str(i.weights))
+    
+print("prediction: " + str(prediction))
+print("actual: " + str(actual))
 
-print(prediction)
-
+NN.calculate_loss(prediction, actual)
 
 
 
